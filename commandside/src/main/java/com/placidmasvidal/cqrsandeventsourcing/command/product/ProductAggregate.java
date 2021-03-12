7b67c9ac036032dc9ebb802628db514103ba7bff
Placid Masvidal <pmasvidal@hillogy.com>
2021-03-12 15:21:46 +0100

    UpdateProduct implemented

--- commandside/src/main/java/com/placidmasvidal/cqrsandeventsourcing/command/product/ProductAggregate.java
+++ commandside/src/main/java/com/placidmasvidal/cqrsandeventsourcing/command/product/ProductAggregate.java
@@ -2,22 +2,45 @@ package com.placidmasvidal.cqrsandeventsourcing.command.product;
 
 import com.placidmasvidal.cqrsandeventsourcing.command.addproduct.AddProductCommand;
 import com.placidmasvidal.cqrsandeventsourcing.command.addproduct.AddProductEvent;
+import com.placidmasvidal.cqrsandeventsourcing.command.updateproduct.UpdateProductCommand;
+import com.placidmasvidal.cqrsandeventsourcing.command.updateproduct.UpdateProductEvent;
+import lombok.extern.slf4j.Slf4j;
 import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.modelling.command.AggregateIdentifier;
 import org.axonframework.spring.stereotype.Aggregate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import static org.axonframework.modelling.command.AggregateLifecycle.apply;
+
+@Slf4j
 @Aggregate
+@Entity
 public class ProductAggregate {
 
     @AggregateIdentifier
+    @Id
     private Long id;
+
+    @Column
     private String name;
+
+    @Column
     private int quantity;
 
     @CommandHandler
     public ProductAggregate(AddProductCommand cmd) {
-        // Verifies state consistency and applies events
+        apply(new AddProductEvent(cmd.getId(), cmd.getName(), cmd.getQuantity()));
+    }
+
+    @CommandHandler
+    public void handle(UpdateProductCommand cmd) {
+        log.info("Handling command {}: {}", cmd.getClass().getSimpleName(), cmd);
+        apply(new UpdateProductEvent(cmd.getId(), cmd.getQuantity()));
+        log.info("Done handling command {}: {}", cmd.getClass().getSimpleName(), cmd);
     }
 
     @EventSourcingHandler
@@ -26,5 +49,11 @@ public class ProductAggregate {
         this.name = event.getName();
         this.quantity = event.getQuantity();
     }
-}
 
+    @EventSourcingHandler
+    public void on(UpdateProductEvent event) {
+        this.quantity = event.getQuantity();
+    }
+
+    protected ProductAggregate() { }
+}
---
+++ commandside/src/main/java/com/placidmasvidal/cqrsandeventsourcing/command/updateproduct/UpdateProductCommand.java
@@ -0,0 +1,23 @@
+package com.placidmasvidal.cqrsandeventsourcing.command.updateproduct;
+
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+public class UpdateProductCommand {
+
+    public UpdateProductCommand(Long id, int quantity) {
+        this.id = id;
+        this.quantity = quantity;
+    }
+
+    @TargetAggregateIdentifier
+    private final Long id;
+    private final int quantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
