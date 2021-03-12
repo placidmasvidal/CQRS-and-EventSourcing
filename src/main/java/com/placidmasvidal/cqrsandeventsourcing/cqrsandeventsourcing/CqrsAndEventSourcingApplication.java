0f1eb433e08f30747a567266ea5d89fd5d6aa780
Placid Masvidal <pmasvidal@hillogy.com>
2021-03-12 10:39:42 +0100

    Setting up project
    added gradle dependencies

---
+++ src/main/java/com/placidmasvidal/cqrsandeventsourcing/cqrsandeventsourcing/CqrsAndEventSourcingApplication.java
@@ -0,0 +1,13 @@
+package com.placidmasvidal.cqrsandeventsourcing.cqrsandeventsourcing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsAndEventSourcingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CqrsAndEventSourcingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/placidmasvidal/cqrsandeventsourcing/cqrsandeventsourcing/CqrsAndEventSourcingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.placidmasvidal.cqrsandeventsourcing.cqrsandeventsourcing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsAndEventSourcingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
