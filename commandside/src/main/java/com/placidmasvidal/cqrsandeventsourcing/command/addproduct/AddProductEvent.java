4e929eed028521e51d8d4f68b2865d9c816cfc85
Placid Masvidal <pmasvidal@hillogy.com>
2021-03-12 12:35:08 +0100

    AddProductEvent added
    also configured docker-compose

---
+++ commandside/src/main/java/com/placidmasvidal/cqrsandeventsourcing/command/addproduct/AddProductEvent.java
@@ -0,0 +1,30 @@
+package com.placidmasvidal.cqrsandeventsourcing.command.addproduct;
+
+public class AddProductEvent {
+
+    public AddProductEvent(
+            final Long id,
+            final String name,
+            final int quantity) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    private final Long id;
+    private final String name;
+    private final int quantity;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
--- commandside/src/main/java/com/placidmasvidal/cqrsandeventsourcing/command/product/ProductAggregate.java
+++ commandside/src/main/java/com/placidmasvidal/cqrsandeventsourcing/command/product/ProductAggregate.java
@@ -1,7 +1,9 @@
 package com.placidmasvidal.cqrsandeventsourcing.command.product;
 
 import com.placidmasvidal.cqrsandeventsourcing.command.addproduct.AddProductCommand;
+import com.placidmasvidal.cqrsandeventsourcing.command.addproduct.AddProductEvent;
 import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.modelling.command.AggregateIdentifier;
 import org.axonframework.spring.stereotype.Aggregate;
 
@@ -16,6 +18,13 @@ public class ProductAggregate {
     @CommandHandler
     public ProductAggregate(AddProductCommand cmd) {
         // Verifies state consistency and applies events
+    }
+
+    @EventSourcingHandler
+    public void on(AddProductEvent event) {
+        this.id = event.getId();
+        this.name = event.getName();
+        this.quantity = event.getQuantity();
     }
 }
 
