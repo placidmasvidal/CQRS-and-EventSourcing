71727e4d8fa97fb2cac1aede00fbea0e0cf16dcc
Placid Masvidal <pmasvidal@hillogy.com>
2021-03-12 11:18:06 +0100

    Added AxonConfig for mongo
    Warning: using deprecated org.axonframework.extensions.mongo extensions

---
+++ commandside/src/main/java/com/placidmasvidal/cqrsandeventsourcing/command/AxonConfig.java
@@ -0,0 +1,26 @@
+package com.placidmasvidal.cqrsandeventsourcing.command;
+
+import com.mongodb.MongoClient;
+import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
+import org.axonframework.extensions.mongo.DefaultMongoTemplate;
+import org.axonframework.extensions.mongo.eventsourcing.eventstore.MongoEventStorageEngine;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AxonConfig {
+
+    // The `MongoEventStorageEngine` stores each event in a separate MongoDB document
+    @Bean
+    public EventStorageEngine storageEngine(MongoClient client) {
+        return MongoEventStorageEngine
+                .builder()
+                .mongoTemplate(DefaultMongoTemplate
+                        .builder()
+                        .mongoDatabase(client)
+                        .build())
+                .build();
+    }
+}
+
+
